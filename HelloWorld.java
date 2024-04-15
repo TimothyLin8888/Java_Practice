62831d0f360ba5e191bdec1d15485d0155c6ea6d
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-04-15 15:09:20 -0400

    added the recommended try to surround the scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,39 +2,43 @@ import java.util.*;
 import java.io.*;
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("How much loops do you want: ");
-        int numOfLoops = scan.nextInt();
-        String message = "Hello World.";
-        String numberedMessage;
-        try {
-            FileOutputStream fs = new FileOutputStream("Hello Worlds.txt");
-            PrintWriter pw = new PrintWriter(fs);
-        for (int i = 1; i <= numOfLoops; i++){
-            String stringNumber = String.valueOf(i); //this turns the integer into a string
-            stringNumber = stringNumber.substring(Math.max(stringNumber.length() - 2, 0)); //this makes it only do the last two digits of the string
-            int newI = Integer.parseInt(stringNumber); //then turns it back into an integer
-            if (newI == 11 || newI == 12 || newI == 13){
-                numberedMessage = i + "th " + message;
-            } else if (i % 10 == 1){
-                numberedMessage = i + "st " + message;
-                } else if (i % 10 == 2){
-                numberedMessage = i + "nd " + message;
-                } else if (i % 10 == 3){
-                numberedMessage = i + "rd " + message;
-                } else {
-                numberedMessage = i + "th " + message;
-                }
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("How much loops do you want: ");
+            int numOfLoops = scan.nextInt();
+            String message = "Hello World.";
+            String numberedMessage;
+            try {
+                FileOutputStream fs = new FileOutputStream("Hello Worlds.txt");
+                PrintWriter pw = new PrintWriter(fs);
+            for (int i = 1; i <= numOfLoops; i++){
+                String stringNumber = String.valueOf(i); //this turns the integer into a string
+                stringNumber = stringNumber.substring(Math.max(stringNumber.length() - 2, 0)); //this makes it only do the last two digits of the string
+                int newI = Integer.parseInt(stringNumber); //then turns it back into an integer
+                if (newI == 11 || newI == 12 || newI == 13){
+                    numberedMessage = i + "th " + message;
+                } else if (i % 10 == 1){
+                    numberedMessage = i + "st " + message;
+                    } else if (i % 10 == 2){
+                    numberedMessage = i + "nd " + message;
+                    } else if (i % 10 == 3){
+                    numberedMessage = i + "rd " + message;
+                    } else {
+                    numberedMessage = i + "th " + message;
+                    }
+                
+            pw.write(numberedMessage + "\n");
+            }
+            pw.close();
+            fs.close();
+   } catch (FileNotFoundException fnf){
+            System.out.println("File not found.");
+   } catch (IOException io){
+            System.out.println("An IO exception");
             
-        pw.write(numberedMessage + "\n");
+   }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        pw.close();
-        fs.close();
-    } catch (FileNotFoundException fnf){
-        System.out.println("File not found.");
-    } catch (IOException io){
-        System.out.println("An IO exception");
-        
-    }
 }
 }
